083c3348eced090531af40e7a0bb8e2f7943b896
Manish Kumar <mk771996@gmail.com>
2026-03-27 15:13:09 +0530

    Update solution for expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -6,12 +6,6 @@ def expected_value_discrete(x, p):
     """
     # Write code here
     x,p=np.array(x),np.array(p)
-    # prob_sum=np.sum(p)
-    # print(prob_sum)
-    # print(np.allclose(np.sum(p),1))
-    # # if np.allclose(prob_sum,1):
-    # tmp=p*x
-    # print(tmp)
     if not np.allclose(np.sum(p),1):
         raise ValueError
     return np.sum(p*x)
