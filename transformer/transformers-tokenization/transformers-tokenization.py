b4985c199873988154fbe41095039d2051938957
Manish Kumar <mk771996@gmail.com>
2026-04-23 16:20:33 +0530

    Add solution for transformers-tokenization

---
+++ transformer/transformers-tokenization/transformers-tokenization.py
@@ -0,0 +1,85 @@
+import numpy as np
+from typing import List, Dict
+
+class SimpleTokenizer:
+    """
+    A word-level tokenizer with special tokens.
+    """
+    
+    def __init__(self):
+        self.word_to_id: Dict[str, int] = {}
+        self.id_to_word: Dict[int, str] = {}
+        self.vocab_size = 0
+        
+        # Special tokens
+        self.pad_token = "<PAD>"
+        self.unk_token = "<UNK>"
+        self.bos_token = "<BOS>"
+        self.eos_token = "<EOS>"
+    
+    def build_vocab(self, texts: List[str]) -> None:
+        """
+        Build vocabulary from a list of texts.
+        Add special tokens first, then unique words.
+        """
+        self.vocab={}
+        self.all_word=set()
+        for text in texts:
+            for word in text.lower().split():
+                self.all_word.add(word)
+        self.vocab[self.pad_token]=0
+        self.vocab[self.unk_token]=1
+        self.vocab[self.bos_token]=2
+        self.vocab[self.eos_token]=3
+
+        self.all_word_s=sorted(self.all_word)
+        
+        # for index, word in enumerate(self.all_word_s):
+        #     self.vocab[word]=index+4
+
+        # self.vocab_size=len(self.vocab)
+        unique_words=self.all_word_s
+        # 1. Assign IDs to special tokens
+        special_tokens = [self.pad_token, self.unk_token, self.bos_token, self.eos_token]
+        for idx, token in enumerate(special_tokens):
+            self.word_to_id[token] = idx
+            
+        # 2. Assign IDs to unique words (starting from index 4)
+        for idx, word in enumerate(sorted(unique_words)):
+            self.word_to_id[word] = idx + len(special_tokens)
+
+        # 3. Create the reverse mapping for decoding
+        self.id_to_word = {v: k for k, v in self.word_to_id.items()}
+        self.vocab_size = len(self.word_to_id)
+        
+    def encode(self, text: str) -> List[int]:
+        """
+        Convert text to list of token IDs.
+        Use UNK for unknown words.
+        """
+        self.encode_ids=[]
+        for word in text.lower().split():
+            if word not in self.word_to_id:
+                self.encode_ids.append(1)
+            else:
+                id=self.word_to_id[word]
+                self.encode_ids.append(id)
+        print(self.vocab)
+        return self.encode_ids
+    
+    def decode(self, ids: List[int]) -> str:
+        """
+        Convert list of token IDs back to text.
+        """
+        # self.decode_vocab={}
+        # for key,val in self.vocab:
+        #     self.decode_vocab[val]=key
+        # self.decode_text=""
+        # self.decode_text=self.id_to_word[ids[0]]
+        # for id in ids[1:]:
+        #     # if id=1
+        #     self.decode_text+=" "+self.id_to_word[id]
+        # # print("text:", self.text, "decode:",decode_text)
+        # return decode_text
+        return " ".join(self.id_to_word.get(idx, self.unk_token) for idx in ids)
+        
